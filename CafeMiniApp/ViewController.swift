415ae0253942a1d25eead24eaabae460157cc7dd
jayden Sawyer <jdnswr@gmail.com>
2024-09-23 21:16:37 -0700

    yo

--- CafeMiniApp/ViewController.swift
+++ CafeMiniApp/ViewController.swift
@@ -12,22 +12,56 @@ class ViewController: UIViewController {
     @IBOutlet weak var textView: UITextView!
     @IBOutlet weak var usrTextView: UITextView!
     @IBOutlet weak var textField2: UITextField!
+    @IBOutlet weak var label: UILabel!
     var cart: [String : Int] = [:]
     var menu = ""
-    var food = ["Pizza","chicken","Soup","Hotdog","Bagel"]
-     var price = ["$5","$3","$4","$2.50","$1.75"]
+    var food = ["Pizza","Chicken","Soup","Hotdog","Bagel"]
+    var price = ["$5","$3","$4","$2.50","$1.75"]
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        
         for i in 0..<food.count{
             menu += "\(food[i])  \(price[i]) \n"
         }
         textView.text = menu
     }
-    @IBAction func addoutlet(_ sender: UIButton) {
-        var foods = textField.text!
-        var quantity = textField2.text!
-      
+    func displayCart() {
+        var totalPrice = 0.0
+        var cartDisplay = "Your Cart:\n"
+        
+        for (item, amount) in cart {
+            if let index = food.firstIndex(of: item) {
+                let itemPriceString = price[index].dropFirst() // Remove the '$' from the price
+                if let itemPrice = Double(itemPriceString) { // Convert to Double
+                    let itemTotal = itemPrice * Double(amount)
+                    totalPrice += itemTotal
+                    cartDisplay += "\(item): \(amount) x $\(itemPrice) = $\(itemTotal)\n"
+                }
+            }
+        }
+        
+        cartDisplay += "\nTotal Price: $\(totalPrice)"
+        usrTextView.text = cartDisplay
+        label.text = "Total: $\(totalPrice)"
     }
+
+        @IBAction func addoutlet(_ sender: UIButton) {
+            var foods = textField.text!
+            var quantity = textField2.text!
+            var idk = Int(quantity)!
+            cart[foods] = idk
+           
+            displayCart()
+            
+            if !food.contains(textField.text!) {
+                    label.text = "Please pick a valid item off the menu."
+            } else if food.contains(textField.text!) {
+                    label.text = "This item is already in your cart."
+                }
+            }
+            }
+                
+            
+        
     
-}
+
